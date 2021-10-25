55a5016250431b7361cacd5ad9ebebdaa58c12e9
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-10-25 16:26:05 -0600

    created Task cmpient and props interface to accept text prop

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+interface TaskProps {
+    text: string;
+}
 
+const Task: React.FC<TaskProps> = ({ text }) => {
+
+    return (
+        <View>
+            <Text>{text}</Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Task;
